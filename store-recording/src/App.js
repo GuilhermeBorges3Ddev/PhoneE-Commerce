aa7ab5cbce4f6aa3773ec76e66e9cd42b2f680af
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-02-17 00:38:36 -0300

    Created components: cart, default, details, navbar, product, produc-list

--- store-recording/src/App.js
+++ store-recording/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+
 /* bootstrap.min added */
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+/* importing the components that have been created */
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 class App extends Component {
   render() {
     return (
      <React.Fragment>
-        <h3>hello from app</h3>
+        <Navbar />
+        <ProductList />
+        <Details />
+        <Cart />
+        <Default />
      </React.Fragment>
     );
   }
