93d802abf08af97ab56270532ead8296bad7639e
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-02-17 23:18:49 -0300

    Maked a Redux global state using the apiGlobal concept over the new file context.js

--- store-recording/src/index.js
+++ store-recording/src/index.js
@@ -6,13 +6,19 @@ import App from './App';
 /* Import of route files here and in the App.js */
 import {BrowserRouter as Router} from 'react-router-dom';
 
+/* The high component whart has to be imported is here, it'll encapsulate App. main component*/
+import {ProductProvider} from './context';
+
 import * as serviceWorker from './serviceWorker';
 
 /* Applying here the Router component imported */
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>, document.getElementById('root')
+    <ProductProvider>
+        <Router>
+            <App />
+        </Router>
+    </ProductProvider>, 
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
