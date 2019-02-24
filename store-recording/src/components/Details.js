5af1a993e7d5191838313f0f60d6ab03263fb71a
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-02-24 16:54:03 -0300

    Basic structure of Details component is done

--- store-recording/src/components/Details.js
+++ store-recording/src/components/Details.js
@@ -12,7 +12,56 @@ export default class Details extends Component {
     return (
       <ProductConsumer>
         {value => {
-          console.log(value.detailProduct);
+          const {id, company, img, info, price, title, inCart} = value.detailProduct;
+          return(
+            <div className="container py-5">
+            {/* title */}
+            <div className="row">
+              <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                <h1>{title}</h1>
+              </div>
+            </div>
+            {/* end of title */}
+            {/* product info */}
+            <div className="row">
+              <div className="col-10 mx-auto col-md-6 my-3">
+                <img src={img} className="img-fluid" alt="product" />
+              </div>
+              {/* product text */}
+              <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                <h2>model : {title}</h2>
+                <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                  made by : <span className="text-uppercase">{company}</span>
+                </h4>
+                <h4 className="text-blue">
+                  <strong>
+                    price : <span>$</span>
+                    {price}
+                  </strong>
+                </h4>
+                <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                  some info about product:
+                </p>
+                <p className="text-muted lead">{info}</p>
+                {/* buttons */}
+                <div>
+                  <Link to='/'>
+                    <ButtonContainer>
+                      back to Products
+                    </ButtonContainer>
+                  </Link>
+                  <ButtonContainer disabled={inCart ? true : false} onClick={() => {
+                    value.addToCart(id)
+                  }}>
+                      {inCart ? "inCart" : "add to cart"}
+                  </ButtonContainer>
+                </div>
+                {/* end of buttons */}
+              </div>
+            </div>
+            {/* end of product info */}
+            </div>
+          )
         }}
       </ProductConsumer>
     )
