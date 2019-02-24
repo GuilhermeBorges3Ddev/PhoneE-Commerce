c98ebb8d7fd0c0c5de4ae1f49156356c752a293d
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-02-24 16:16:28 -0300

    Imported components to work sincronyzing the Details component with data.js, as ProductConsumer and Link

--- store-recording/src/components/Details.js
+++ store-recording/src/components/Details.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
+/* As well, we'll consume the stucture passed by Provider component */
+import { ProductConsumer } from '../context';
+
+/* Importing the Link component to make the 2 bottom butons work as we expect */
+import { Link } from 'react-router-dom';
+import { ButtonContainer } from './Button';
+
 export default class Details extends Component {
   render() {
     return (
-      <div>
-        <h3>hello from details</h3>
-      </div>
+      <ProductConsumer>
+        {value => {
+          console.log(value.detailProduct);
+        }}
+      </ProductConsumer>
     )
   }
 }
