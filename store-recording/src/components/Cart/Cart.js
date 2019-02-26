ab10bf82d21d865f249c647e0c3672a1f6ddef0c
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-02-25 23:57:27 -0300

    Subcomponents CartColumns.js and EmptyCart.js are done and mixed with the main component Cart.js

--- store-recording/src/components/Cart/Cart.js
+++ store-recording/src/components/Cart/Cart.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Title from '../Title';
+import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
+import {ProductConsumer} from '../../context';
 
 export default class Cart extends Component {
   render() {
     return (
-      <div>
-        <h3>hello from cart</h3>
-      </div>
-    )
+      <section>
+        <ProductConsumer>
+          {value => {
+            const {cart} = value;
+            if(cart.lenght > 0){
+              return(
+                 <React.Fragment>
+                   <Title name="your" title="cart" />
+                  <CartColumns />
+                </React.Fragment>  
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
+      </section>
+    );
   }
 }
